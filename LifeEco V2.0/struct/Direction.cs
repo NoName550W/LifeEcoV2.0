6e4fdf4bf3f2c0ef04cfa89b2b5279825cd18c62
NoName550W <kotapastekom@gmail.com>
2022-05-31 17:33:16 +0300

    направление

---
+++ LifeEco V2.0/struct/Direction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+public struct Direction
+{
+	public Direction(int directionM = -1)
+	{
+		if (directionM < 0)
+        {
+			if (directionM > 7)
+			{
+				direction = directionM - 8;
+			}
+			else if (directionM < 0)
+			{
+				direction = directionM + 8;
+			}
+			else
+			{
+				direction = directionM;
+			}
+		} 
+		else
+        {
+			Random rnd = new Random();
+			int directionR = rnd.Next(0, 8);
+			direction = directionR;
+		}
+		directionBack = (direction + 4) % 8;
+
+	}
+
+	public void L()
+	{
+		direction--;
+
+		if (direction < 0)
+		{
+			direction += 8;
+		}
+
+		directionBack = (direction + 4) % 8;
+	}
+
+	public void R()
+	{
+		direction++;
+
+		if (direction > 7)
+		{
+			direction -= 8;
+		}
+
+		directionBack = (direction + 4) % 8;
+	}
+
+	public int direction;
+	public int directionBack;
+}
